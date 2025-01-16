6a5cdefc98ea6f5cd1be3087eec91524996012c5
MohammadShujaullah <mdshujaullah789@gmail.com>
2025-01-16 21:49:22 +0530

    promise is asyntax which is used in place of callback ,foe clean code ,here you can see this

--- Use_Of_Promises_and_withoutPromises.js
+++ Use_Of_Promises_and_withoutPromises.js
@@ -1,33 +1,49 @@
 //without the use of promises
 
-const fs=require("fs")
+// promise is Asynchronous fn. which is slightly more readble and MAKE CallBacks
 
-function shujaWrite(loaded){
-    fs.readFile("a.text","utf-8",function(err,data){
-          loaded(data)
+const fs = require("fs")
+
+function shujaWrite(loaded) {
+    fs.readFile("a.text", "utf-8", function (err, data) {
+        loaded(data)
     })
 
 }
 
-function upload(data){
+function upload(data) {
     console.log(data);
 
 }
 shujaWrite(upload);
 
-// with the use of promises
+// with the use of promises     ( new keywords for me--> promise,then,resolve)
 
-const fss=require("fs")
+const fss = require("fs")
 
-function shujaWriting(loaded){
-    fss.readFile("a.text","utf-8",function(err,data){
-          loaded(data)
+function shujaWriting() {
+    return new Promise(function (resolve) {
+        fss.readFile("a.text", "utf-8", function (err, data) {
+            resolve(data)
+        })
+
     })
 
 }
 
-function uploading(data){
+function uploading(data) {
     console.log(data);
 
 }
-shujaWriting(uploading);
+shujaWriting().then(uploading);
+
+// another example is use of promise-------------------------------
+
+const d= new Promise(function(resolve){
+        resolve("hi everyone")
+})
+
+
+d.then(function (data){
+    console.log(data);
+});
