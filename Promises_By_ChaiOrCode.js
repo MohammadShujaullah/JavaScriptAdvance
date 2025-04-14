04276dd4d50e365789029429c07fbc2491724325
MohammadShujaullah <mdshujaullah789@gmail.com>
2025-04-15 01:42:33 +0530

    done the task of fetching , the APIs from google or github

--- Promises_By_ChaiOrCode.js
+++ Promises_By_ChaiOrCode.js
@@ -1,46 +1,76 @@
-const promisefour=new Promise(function(resolve,reject){
-    setTimeout(function(){
-       let error=true
-       if(!error){
-         resolve(username="sirji",password="123")
-       }
-       else{
-        reject("somthing went wrong ")
-       }
-    },2000)
+const promisefour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error =false
+        if (error === false) {
+            resolve({username : "sirji", password : "123"})
+        }
+        else {
+            reject("somthing went wrong ")
+        }
+    }, 2000)
 })
 
-promisefour.then((user)=>{
+promisefour.then((user) => {
     console.log(user);
     return user.username;
 
-}).then((username)=>{
+}).then((username) => {
     console.log(username)
 })
-.catch((rejected)=>{       // when reject occure or error occures then .catch atta ha , saare then fir ht jaate han
-    console.log(rejected)
-})
-.finally(console.log("the promise is either resolve or reject"))
+    .catch((rejected) => {       // when reject occure or error occures then .catch atta ha , saare then fir ht jaate han
+        console.log(rejected)
+    }).finally(console.log("the promise is either resolve or reject"))
+
 
 
 
 // ***async and await doesnt handle the error , it only work  when resolve occures
-const promisefive=new Promise(function(resolve,reject){
-    setTimeout(function(){
-       let error=false
-       if(!error){
-         resolve(username="sirji",password="123")
-       }
-       else{
-        reject("somthing went wrong ")
-       }
-    },2000)
+const promisefive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = false
+        if (!error) {
+            resolve({username : "sirji", password :"123"})
+        }
+        else {
+            reject("somthing went wrong ")
+        }
+    }, 2000)
 })
 
-async function asynchro(){
-  try{  const x=await promisefive
-    console.log(x);}
-    catch(error){
-        console.log(error)
+async function asynchro() {
+    try {
+        const x = await promisefive
+        console.log(x);
+    }
+    catch (error) { 
+        console.log(error)    // you have to add catch additionally , in async-await
     }
 }
+asynchro()
+
+
+//////////////////////////////////////////////////////////
+async function getAllUsers(){
+  try {  const response=await fetch('https://jsonplaceholder.typicode.com/users') // phle fetch krna ha 
+    const data=await response.json()     //fir appko json main convert krna hoga 
+                                         // response,json() time leta ha islie , await response.json()
+    console.log(data)}
+    catch{(error)=>{
+            console.log("error happening: ",error)   }
+        
+    }
+
+}
+getAllUsers()
+
+
+////////////////////// 2nd method of above///////////
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response)=>{
+//     return response.json()
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((error)=>{
+//     console.log("error  happened:",error)
+// })
